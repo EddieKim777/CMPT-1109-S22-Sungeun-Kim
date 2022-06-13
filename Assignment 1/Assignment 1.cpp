b141ec78e20dc9be052dbc2691287ad8540db629
EddieKim777 <sung920111@gmail.com>
2022-06-13 16:01:41 -0700

    final

--- Assignment 1/Assignment 1.cpp
+++ Assignment 1/Assignment 1.cpp
@@ -27,32 +27,72 @@ int compute_check_digit(int arr[], int size) {
     return check_digit;
 }
 
+int return_length(int seletion) {
+    
+    if (seletion == 1) {
+        int size = 8;
+        return size;
+    }
+    else if (seletion == 2) {
+        int size = 12;
+        return size;
+    }
+    else if (seletion == 3) {
+        int size = 13;
+        return size;
+    }
+    else if (seletion == 4) {
+        int size = 14;
+        return size;
+    }
+    else if (seletion == 5) {
+        int size = 18;
+        return size;
+    }
+
+
+    
+}
+
+
+
 
 
 int main()
 {
     int arr[20];
-    int size = 0;
-    int digit = 0;
-    cout << "type the size of barcode" << endl;
-        cin >> size;
+    int selection = 0;
+    
+   
+   
+    cout << "Select barcode type" << endl;
+    cout << "1. EAN-8" << endl;
+    cout << "2. UPC-A" << endl;
+    cout << "3. EAN-13" << endl;
+    cout << "4. EAN-14" << endl;
+    cout << "5. SSCC" << endl;
+    cin >> selection;
+
+    return_length(selection);
+
+          
     
     cout << "type barcode digit by digit" << endl;
     
     
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < return_length(selection) - 1 ; i++) {
         cin >> arr[i];
         
             }
 
     cout << "input code is ";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < return_length(selection) - 1; i++)
         cout << arr[i];
     cout << endl;
     cout << "compute result is ";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < return_length(selection) - 1; i++)
         cout << arr[i];
-    cout << compute_check_digit(arr, size);
+    cout << compute_check_digit(arr, return_length(selection)-1);
 
 
     return 0;
